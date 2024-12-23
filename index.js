fa12b0922c7fd749ff29f5f4dbb9cb25a497a705
maximomendz <maximomendz87@gmail.com>
2024-12-23 08:05:47 +0000

    Configured passport for the user

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { buildContext } from "graphql-passport";
 import { connectDb } from "./db/connectDb.js";
+import { configurePassport } from "./passport/passport.js";
 import mergeSchemasArray from "./schemas/index.js";
 import mergeResolversArray from "./resolvers/index.js";
 
@@ -9,11 +11,40 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
 
 dotenv.config();
-const app = express();
+configurePassport();
 
+const app = express();
 const httpServer = http.createServer(app);
+const MongoDBStore = new connectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  databaseName: 'budgetTracker',
+  collection: "sessions",
+});
+
+store.on("Store Error", (error) => console.log(error));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // A secret key used to sign the session ID cookie
+    resave: false, // Don't save session if unmodified
+    saveUninitialized: false, // Don't create session until something stored
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+      httpOnly: true, // The cookie only accessible by the web server
+    },
+    store: store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergeSchemasArray,
@@ -25,10 +56,13 @@ await server.start();
 
 app.use(
   "/",
-  cors(),
+  cors({
+    origin: "http://localhost:3000", // Allow the React app to connect to the server
+    credentials: true, // Enable cookies
+  }),
   express.json(),
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req, res }) => buildContext({ req, res }), // Pass the request to the resolvers
   })
 );
 
